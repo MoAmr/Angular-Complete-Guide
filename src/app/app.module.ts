b29e75e4652a46cf13a12604ed79c0daea17e32b
mohamed amr <mohamed.amr@asset.com.eg>
2019-04-08 19:29:55 +0200

    Adding recipe button to add new Recipe. closes #8

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,7 +10,7 @@ import {RecipeDetailComponent} from './recipes/recipe-detail/recipe-detail.compo
 import {RecipeItemComponent} from './recipes/recipe-list/recipe-item/recipe-item.component';
 import {ShoppingListComponent} from './shopping-list/shopping-list.component';
 import {ShoppingEditComponent} from './shopping-list/shopping-edit/shopping-edit.component';
-import {FormsModule} from '@angular/forms';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {DropdownDirective} from './shared/dropdown.directive';
 import {ShoppingListService} from './shopping-list/shopping-list.service';
 import {RecipeStartComponent} from './recipes/recipe-start/recipe-start.component';
@@ -34,6 +34,7 @@ import {RecipeEditComponent} from './recipes/recipe-edit/recipe-edit.component';
     BrowserModule,
     AppRoutingModule,
     FormsModule,
+    ReactiveFormsModule,
     AppRoutingModule
   ],
   providers: [ShoppingListService],
